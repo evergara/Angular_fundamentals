0449b8e0b867594318206ca1fb83e7ab1aabec5b
Emerson Vergara <ee-web@hotmail.com>
2022-08-24 11:06:59 -0500

    Se quita todo la creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { InlineComponent } from './inline.component';
-import { HelloComponent } from './hello/hello.component';
-import { PersonComponent } from './person/person.component';
 
 @NgModule({
-  declarations: [AppComponent, InlineComponent, HelloComponent, PersonComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
