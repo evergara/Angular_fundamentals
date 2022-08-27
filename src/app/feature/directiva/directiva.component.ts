69b6ab293b80eb936a067511fa8f7f26e3dcecc2
Emerson Vergara <ee-web@hotmail.com>
2022-08-27 08:21:17 -0500

    ngClass

--- src/app/feature/directiva/directiva.component.ts
+++ src/app/feature/directiva/directiva.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directiva',
   templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.scss']
+  styleUrls: ['./directiva.component.scss'],
 })
 export class DirectivaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  
+  UpdateNameClass(count: number) {
+    return {
+      error: count <= 2,
+      warning: count > 2 && count <= 4,
+      success: count === 5,
+      bold: count === 5,
+    };
   }
-
 }
